98988a943dcfe1d456a2a6bff580eb1a64bc4d28
Andreea <amuresan@ualberta.ca>
2020-11-04 00:02:11 -0700

    base of simulate

---
+++ assignment3/Gomoku3.py
@@ -0,0 +1,90 @@
+#!/usr/local/bin/python3
+# /usr/bin/python3
+# Set the path to your python3 above
+
+from gtp_connection import GtpConnection
+from board_util import GoBoardUtil, PASS, EMPTY, BLACK, WHITE
+from board import GoBoard
+import numpy as np
+
+class Gomoku():
+    def __init__(self):
+        """
+        Gomoku player that selects moves randomly from the set of legal moves.
+        Passes/resigns only at the end of the game.
+
+        Parameters
+        ----------
+        name : str
+            name of the player (used by the GTP interface).
+        version : float
+            version number (used by the GTP interface).
+        """
+        self.name = "GomokuAssignment3"
+        self.version = 1.0
+        self.numSimulations = 10
+
+    def get_move(self, board, color):
+        # generate a move using one-ply MC simulations
+        boardCopy = board.copy()
+        # all empyt points are legal in Gomoku
+        # TODO: change to follow rules
+        legalMoves = board.get_empty_points()
+
+        # no more legal moves to pick from
+        if legalMoves == []:
+            return None
+
+        # number of times the move has won
+        numMoveWins = []
+        for move in legalMoves:
+            wins = self.simulate_move(boardCopy, move, color)
+            numMoveWins.append(wins)
+
+        # select the best move
+        max_child = np.argmax(numMoveWins)
+        return legalMoves[max_child]
+
+    def simulate_move(self, board, move, color):
+        wins = 0
+        for _ in range(self.numSimulations):
+            result = self.simulate(board, move, color)
+            if result == color:
+                win += 1
+        return wins
+
+    def simulate(self, board, move, color):
+        boardCopy = board.copy()
+        boardCopy.play_move()
+        # first player played move, now opponent plays
+        opponent = GoBoardUtil.opponent(color)
+        return self.play_game(boardCopy, opponent)
+
+    def play_game(self, board, color):
+        passes = 0
+        # simulate entire game to completion
+        while board.detect_five_in_a_row() == EMPTY || board.get_empty_points() != []:
+            color = board.current_player
+            # TODO: filter moves by rules
+            move = GoBoardUtil.generate_random_move(board, color)
+            board.play_move(move, color)
+            if move == PASS:
+                passes += 1
+            else:
+                passes = 0 # reset number of consecutive passes
+            if passes >= 2:
+                break
+        # return the winning colour
+        return board.detect_five_in_a_row()
+
+def run():
+    """
+    start the gtp connection and wait for commands.
+    """
+    board = GoBoard(7)
+    con = GtpConnection(Gomoku(), board)
+    con.start_connection()
+
+
+if __name__ == "__main__":
+    run()
